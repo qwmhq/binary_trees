43279123e2f0b725e8065e1b87805433b42e173f
Quwam Ogunlaja <quwam32@gmail.com>
2023-02-28 16:13:45 +0100

    task 9 - binary_tree_height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * max - find the greater of two integers
+ *
+ * @n1: integer
+ * @n2: integer
+ *
+ * Return: the greater value between n1 and n2
+ */
+size_t max(size_t n1, size_t n2)
+{
+	if (n1 > n2)
+		return (n1);
+	return (n2);
+}
+
+/**
+ * binary_tree_height - measure the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: the height of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (1 + max(left_height, right_height));
+}
